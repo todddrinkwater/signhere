4c042cb806f0cfb7b1cdbc8242801eb69779338b
todddrinkwater <todddrinkwaternz@gmail.com>
2017-04-28 12:17:40 +1200

    add contracts get route

--- client/actions/index.js
+++ client/actions/index.js
@@ -8,7 +8,22 @@ export const getUserDetails = loggedInUserDetails => {
 }
 
 export const loggedInUser = (user, dispatch) => {
-  console.log(dispatch)
+  request
+  .get('/user/profile/' + user.id)
+  .end((err, res) => {
+    var userInfo = JSON.parse(res.text)
+    console.log(userInfo[0], "+ res")
+    if (err) {
+      console.error('loggedInUser ' + err.message)
+      return
+    }
+    dispatch(getUserDetails(userInfo[0]))
+    console.log("return")
+    })
+
+}
+
+export const getUserContracts = (user, dispatch) => {
   request
   .get('/user/profile/' + user.id)
   .end((err, res) => {
