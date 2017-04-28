d25158af18cb02fd61659fcc0f16eb59e4f623c4
todddrinkwater <todddrinkwaternz@gmail.com>
2017-04-28 12:00:41 +1200

    change route path to user/profile/ for profile info

--- client/actions/index.js
+++ client/actions/index.js
@@ -10,7 +10,7 @@ export const getUserDetails = loggedInUserDetails => {
 export const loggedInUser = (user, dispatch) => {
   console.log(dispatch)
   request
-  .get('/user/' + user.id)
+  .get('/user/profile/' + user.id)
   .end((err, res) => {
     var userInfo = JSON.parse(res.text)
     console.log(userInfo[0], "+ res")
--- server/routes/userById.js
+++ server/routes/userById.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 var db = require('../db')
 
-router.get('/:id', function (req, res) {
+router.get('/profile/:id', function (req, res) {
   var id = req.params.id
   db.getUserById(id).then((result) => {
     res.send(result)
