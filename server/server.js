124064e64388914fd943e19f33f961bfa9aeefa4
todddrinkwater <todddrinkwaternz@gmail.com>
2017-07-02 19:54:25 +1200

    add cors module

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 var path = require('path')
 var express = require('express')
 var bodyParser = require('body-parser')
-
 var server = express()
+var cors = require('cors')
 
 var userById = require('./routes/userById')
 
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
-
+server.use(cors())
 server.use('/user/', userById)
 
 module.exports = server
