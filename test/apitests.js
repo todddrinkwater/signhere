3e0e879291a4ff8122e67ebd04fe579ecdc5e96d
todddrinkwater <todddrinkwaternz@gmail.com>
2017-07-20 10:25:07 +1200

    add post tests for creating new contracts

--- test/apitests.js
+++ test/apitests.js
@@ -11,15 +11,8 @@ test('test basic get request', t => {
     })
 })
 
-//will test break if:
-  //there are not the same number of keys being returned?
-  //if some of the keys have undefined values? X
-  //if some of the keys are the same value types as expected? What value types are we expecting? Have we specified?
-  //test that keys are returning the same names. X
 test('test that we can get the correct response object from the /profile route', t => {
   let expected = {'id': 1, 'fName': 'Todd', 'lName': 'Drinkwater', 'organisation': 'Company', 'email': 'todd@drinkwater.com', 'street_address': '70 King St', 'suburb': 'Kelburn', 'phone': 027027027}
-  // place actual test data in here.
-
 
   request(app)
   .get('/user/profile/1')
@@ -42,22 +35,56 @@ test('test that we can get the correct response object from the /profile route',
 
 })
 
-// test('test that we post data to the database to create a new contract', t => {
-//   let contractData = {
-//     id: 1,
-//     signee_id: 2,
-//     contract_header: "SuperTest header",
-//     contract_desc: "SuperTest description"
-//   }
-//
-//   request(app)
-//     .post('/user/contracts/new/1')
-//     .send(contractData)
-//     .expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function(err, res) {
-//       if (err) throw err
-//       t.equal(res.status, 200, 'server responds with 200')
-//       t.end()
-//     })
-// })
+test('test that we can post data to the database to create a new contract', t => {
+  let contractData = {
+    id: 1,
+    signee_id: 2,
+    contract_header: "SuperTest header",
+    contract_desc: "SuperTest description"
+  }
+
+  request(app)
+    .post('/user/contracts/new/1')
+    .send(contractData)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function(err, res) {
+      if (err) throw err
+      t.equal(res.status, 200, 'server responds with 200')
+      // console.log(res)
+      t.end()
+    })
+
+
+})
+
+test('test that post has inserted the correct data', t => {
+
+  let expected = {
+    id: 1,
+    signee_id: 2,
+    contract_header: "SuperTest header",
+    contract_desc: "SuperTest description",
+    signature_url: null,
+    date_signed: null,
+    isSigned: null
+  }
+
+  request(app)
+    .get('/user/contracts/2')
+    .end(function(err, res) {
+      if (err) throw err
+      var newEntry = JSON.parse(res.text)
+      newEntry = newEntry[newEntry.length - 1]
+      t.equal(Object.keys(expected).sort, Object.keys(newEntry).sort, 'both objects have the same keys')
+      t.equal(typeof(expected.id), typeof(newEntry.id), 'contract id value is correct type')
+      t.equal(typeof(expected.signee_id), typeof(newEntry.signee_id), 'contract signee_id value is correct type')
+      t.equal(typeof(expected.contract_header), typeof(newEntry.contract_header), 'contract_header value is correct type')
+      t.equal(typeof(expected.contract_desc), typeof(newEntry.contract_desc), 'contract_desc value is correct type')
+      t.equal(typeof(expected.signature_url), typeof(newEntry.signature_url), 'contract signature_url value is correct type')
+      t.equal(typeof(expected.date_signed), typeof(newEntry.date_signed), 'contract date_signed value is correct type')
+      t.equal(typeof(expected.isSigned), typeof(newEntry.isSigned), 'contract isSigned value is correct type')
+      t.end()
+    })
+
+})
